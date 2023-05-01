2c210e0512602d35b42e70b3043fe22bdc94d4e9
DiegoBandhy <diegoby1994@gmail.com>
2023-05-01 12:48:32 -0500

    crear usuario funciona, revisar auth

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -1,13 +1,27 @@
 import dotenv from 'dotenv'
 import { AdminEmail } from "../config/config.js";
 
+dotenv.config()
+
 export default function auth(req, res, next) {
-    if (req.session.login === AdminEmail) {
+    const { username , email, password } = req.body;   
+    
+    if (username === process.env.USER && password === process.env.ADMIN_PASSWORD && email === process.env.ADMIN_EMAIL) {
+        req.session.login = username;
         next();
     } else {
         return res.status(401).send('No autorizado');
     }
 }
+
+// export default function auth(req, res, next) {
+    
+//     if (req.session.login === AdminEmail) {
+//         next();
+//     } else {
+//         return res.status(401).send('No autorizado');
+//     }
+// }
 
 // import { AdminEmail } from "../config/config.js"
 
